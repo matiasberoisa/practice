ff8460f2aa1a90f1fd535367c7421bf607077144
matias <mberoisa9@gmail.com>
2020-11-23 20:47:15 -0300

    primer commit

---
+++ practice/Assets/scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool canJump;
+    
+    // Start is called before the first frame update
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+         if(Input.GetKey("left"))  
+         {
+             gameObject.transform.Translate(-20f * Time.deltaTime, 0, 0);
+         }
+        if(Input.GetKey("right"))  
+         {
+             gameObject.transform.Translate(20f * Time.deltaTime, 0, 0);
+         }
+        ManageJump();
+    }
+    
+    void ManageJump()
+    {
+        if(gameObject.transform.position.y <= 0)
+        {
+            canJump = true;
+        }
+        
+        if(Input.GetKey("up") && canJump && gameObject.transform.position.y < 10)
+        {
+            gameObject.transform.Translate(0, 20f * Time.deltaTime, 0);
+        }
+        else
+        {
+            canJump = false;
+
+            if(gameObject.transform.position.y > 0)
+            {
+                gameObject.transform.Translate(0, -20f * Time.deltaTime, 0);
+            }
+        }
+    }
+}
