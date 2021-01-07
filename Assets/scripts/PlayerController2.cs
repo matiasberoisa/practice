1fba0b0d3cc55efcdb62d98c5156c2a0bb901cc5
matias <mberoisa9@gmail.com>
2021-01-06 21:30:18 -0300

    agregue movimientos y el personaje y las monedas

--- Assets/scripts/PlayerController2.cs
+++ Assets/scripts/PlayerController2.cs
@@ -15,12 +15,20 @@ public class PlayerController2 : MonoBehaviour
     void Update(){
         if (Input. GetKey("left"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000f * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
         if (Input. GetKey("right"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000f * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
+        if (!Input. GetKey("left") && !Input. GetKey("right"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("moving", false);
+            }
         if(Input. GetKeyDown("up") && canJump)
         {
             canJump = false;
